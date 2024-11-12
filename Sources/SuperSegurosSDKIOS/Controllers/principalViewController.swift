0099ac938f0f55b08c84c6a803b7dece5b1002d5
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-12 12:39:05 -0600

    fix .... first commit

---
+++ Sources/SuperSegurosSDKIOS/Controllers/principalViewController.swift
@@ -0,0 +1,45 @@
+//
+//  principalViewController.swift
+//  SuperSegurosSDKIOS
+//
+//  Created by Oscar Aguilar on 12/11/24.
+//
+
+import UIKit
+
+class principalViewController: UIViewController {
+
+    public static let principalVC = UIStoryboard(name: "Storyboard", bundle: Bundle.module).instantiateInitialViewController()!
+    
+    @IBOutlet weak var quoteInsuranceTopButton: UIButton!
+    @IBOutlet weak var quoteInsuranceBottomButton: UIButton!
+    @IBOutlet weak var typeInsuranceSegmentedControl: UISegmentedControl!
+    
+    @IBAction func backAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func quoteInsuranceAction(_ sender: Any) {
+        print("READY......")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setStyle()
+    }
+    
+    func setStyle() {
+        quoteInsuranceTopButton.layer.cornerRadius = 20
+        quoteInsuranceBottomButton.layer.cornerRadius = 20
+        
+        typeInsuranceSegmentedControl.backgroundColor = .white
+        typeInsuranceSegmentedControl.layer.borderColor = UIColor.lightGray.cgColor
+        if #available(iOS 13.0, *) {
+            typeInsuranceSegmentedControl.selectedSegmentTintColor = .white
+        }
+        typeInsuranceSegmentedControl.layer.borderWidth = 1
+        
+    }
+    
+}
