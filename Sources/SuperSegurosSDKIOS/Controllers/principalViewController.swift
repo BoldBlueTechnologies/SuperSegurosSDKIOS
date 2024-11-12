a6f21b1bd3ecba3507c240d25613c9a9ad427a1e
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-12 15:11:35 -0600

    fixed ......

--- Sources/SuperSegurosSDKIOS/Controllers/principalViewController.swift
+++ Sources/SuperSegurosSDKIOS/Controllers/principalViewController.swift
@@ -15,12 +15,65 @@ public class principalViewController: UIViewController {
     @IBOutlet weak var quoteInsuranceBottomButton: UIButton!
     @IBOutlet weak var typeInsuranceSegmentedControl: UISegmentedControl!
     
+    //views include insurance
+    @IBOutlet weak var OneView: UIView!
+    @IBOutlet weak var TwoView: UIView!
+    @IBOutlet weak var ThreeView: UIView!
+    @IBOutlet weak var FourView: UIView!
+    @IBOutlet weak var FiveView: UIView!
+    @IBOutlet weak var SixView: UIView!
+    @IBOutlet weak var SevenView: UIView!
+    @IBOutlet weak var EigthView: UIView!
+    @IBOutlet weak var NineView: UIView!
+    @IBOutlet weak var TenView: UIView!
+    
+    
     @IBAction func backAction(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func quoteInsuranceAction(_ sender: Any) {
         print("READY......")
+    }
+    
+    @IBAction func typeInsuranceSC(_ sender: UISegmentedControl) {
+        switch typeInsuranceSegmentedControl.selectedSegmentIndex {
+        case 0:
+            print("case 0")
+            
+            OneView.isHidden = false
+            TwoView.isHidden = false
+            ThreeView.isHidden = false
+            FourView.isHidden = false
+            
+        case 1:
+            print("case 1")
+            OneView.isHidden = true
+            
+            TwoView.isHidden = false
+            ThreeView.isHidden = false
+            FourView.isHidden = false
+            
+        case 2:
+            print("case 2")
+            
+            OneView.isHidden = true
+            TwoView.isHidden = true
+            ThreeView.isHidden = true
+            
+            FourView.isHidden = false
+            
+        case 3:
+            print("case 3")
+            
+            OneView.isHidden = true
+            TwoView.isHidden = true
+            ThreeView.isHidden = true
+            FourView.isHidden = true
+            
+        default:
+            print("default")
+        }
     }
     
     public override func viewDidLoad() {
@@ -34,11 +87,13 @@ public class principalViewController: UIViewController {
         quoteInsuranceTopButton.layer.cornerRadius = 20
         quoteInsuranceBottomButton.layer.cornerRadius = 20
         
-        typeInsuranceSegmentedControl.backgroundColor = .white
+        let titleSelected = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        let titleUnselected = [NSAttributedString.Key.foregroundColor: UIColor.black]
+        
+        typeInsuranceSegmentedControl.setTitleTextAttributes(titleUnselected, for: .normal)
+        typeInsuranceSegmentedControl.setTitleTextAttributes(titleSelected, for: .selected)
+        
         typeInsuranceSegmentedControl.layer.borderColor = UIColor.lightGray.cgColor
-        if #available(iOS 13.0, *) {
-            typeInsuranceSegmentedControl.selectedSegmentTintColor = .white
-        }
         typeInsuranceSegmentedControl.layer.borderWidth = 1
         
     }
