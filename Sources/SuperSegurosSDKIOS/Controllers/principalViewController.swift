41d700b51ca4cf4dff6edf1ac0c70db8c09bbdf3
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-12 16:20:31 -0600

    fixed .... buttons fonts

--- Sources/SuperSegurosSDKIOS/Controllers/principalViewController.swift
+++ Sources/SuperSegurosSDKIOS/Controllers/principalViewController.swift
@@ -26,15 +26,25 @@ public class principalViewController: UIViewController {
     
     //views include insurance
     @IBOutlet weak var OneView: UIView!
+    @IBOutlet weak var oneTextLabel: UILabel!
     @IBOutlet weak var TwoView: UIView!
+    @IBOutlet weak var twoTextLabel: UILabel!
     @IBOutlet weak var ThreeView: UIView!
+    @IBOutlet weak var threeTextLabel: UILabel!
     @IBOutlet weak var FourView: UIView!
+    @IBOutlet weak var fourTextLabel: UILabel!
     @IBOutlet weak var FiveView: UIView!
+    @IBOutlet weak var fiveTextLabel: UILabel!
     @IBOutlet weak var SixView: UIView!
+    @IBOutlet weak var sixTextLabel: UILabel!
     @IBOutlet weak var SevenView: UIView!
+    @IBOutlet weak var sevenTextLabel: UILabel!
     @IBOutlet weak var EigthView: UIView!
+    @IBOutlet weak var eigthTextLabel: UILabel!
     @IBOutlet weak var NineView: UIView!
+    @IBOutlet weak var nineTextLabel: UILabel!
     @IBOutlet weak var TenView: UIView!
+    @IBOutlet weak var tenTextLabel: UILabel!
     
     
     @IBAction func backAction(_ sender: Any) {
@@ -47,41 +57,34 @@ public class principalViewController: UIViewController {
     
     @IBAction func typeInsuranceSC(_ sender: UISegmentedControl) {
         switch typeInsuranceSegmentedControl.selectedSegmentIndex {
-        case 0:
-            print("case 0")
-            
-            OneView.isHidden = false
-            TwoView.isHidden = false
-            ThreeView.isHidden = false
-            FourView.isHidden = false
-            
-        case 1:
-            print("case 1")
-            OneView.isHidden = true
-            
-            TwoView.isHidden = false
-            ThreeView.isHidden = false
-            FourView.isHidden = false
-            
-        case 2:
-            print("case 2")
-            
-            OneView.isHidden = true
-            TwoView.isHidden = true
-            ThreeView.isHidden = true
-            
-            FourView.isHidden = false
-            
-        case 3:
-            print("case 3")
-            
-            OneView.isHidden = true
-            TwoView.isHidden = true
-            ThreeView.isHidden = true
-            FourView.isHidden = true
-            
-        default:
-            print("default")
+            case 0:
+                OneView.isHidden = false
+                TwoView.isHidden = false
+                ThreeView.isHidden = false
+                FourView.isHidden = false
+                
+            case 1:
+                OneView.isHidden = true
+                
+                TwoView.isHidden = false
+                ThreeView.isHidden = false
+                FourView.isHidden = false
+                
+            case 2:
+                OneView.isHidden = true
+                TwoView.isHidden = true
+                ThreeView.isHidden = true
+                
+                FourView.isHidden = false
+                
+            case 3:
+                OneView.isHidden = true
+                TwoView.isHidden = true
+                ThreeView.isHidden = true
+                FourView.isHidden = true
+                
+            default:
+                print("default")
         }
     }
     
@@ -115,6 +118,19 @@ public class principalViewController: UIViewController {
         titleTwoLabel.font = UIFont(name: "Poppins-Regular", size: 18)
         subTitleTwoLabel.font = UIFont(name: "Poppins-SemiBold", size: 30)
         preTitleLabel.font = UIFont(name: "Poppins-SemiBold", size: 18)
+        
+        oneTextLabel.font = UIFont(name: "Poppins-SemiBold", size: 16)
+        twoTextLabel.font = UIFont(name: "Poppins-Regular", size: 16)
+        threeTextLabel.font = UIFont(name: "Poppins-Regular", size: 16)
+        fourTextLabel.font = UIFont(name: "Poppins-Regular", size: 16)
+        fiveTextLabel.font = UIFont(name: "Poppins-Regular", size: 16)
+        sixTextLabel.font = UIFont(name: "Poppins-Regular", size: 16)
+        sevenTextLabel.font = UIFont(name: "Poppins-Regular", size: 16)
+        eigthTextLabel.font = UIFont(name: "Poppins-Regular", size: 16)
+        nineTextLabel.font = UIFont(name: "Poppins-Regular", size: 16)
+        tenTextLabel.font = UIFont(name: "Poppins-Regular", size: 16)
+        
+        quoteInsuranceBottomButton.titleLabel?.font = UIFont(name: "Poppins-SemiBold", size: 18)
     }
     
     func setStyle() {
@@ -132,6 +148,10 @@ public class principalViewController: UIViewController {
         
         typeInsuranceSegmentedControl.setTitleTextAttributes(titleUnselected as [NSAttributedString.Key : Any], for: .normal)
         typeInsuranceSegmentedControl.setTitleTextAttributes(titleSelected as [NSAttributedString.Key : Any], for: .selected)
+        
+        if #available(iOS 13.0, *) {
+            typeInsuranceSegmentedControl.selectedSegmentTintColor = UIColor(named: "rosaSuper")
+        }
         
         typeInsuranceSegmentedControl.layer.borderColor = UIColor.lightGray.cgColor
         typeInsuranceSegmentedControl.layer.borderWidth = 1
