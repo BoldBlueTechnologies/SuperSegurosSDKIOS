228aba88a299d127a9ae4fadbf09ff2b892e0f31
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-19 16:18:11 -0600

    fixed ...

--- Sources/SuperSegurosSDKIOS/Controllers/selectInsuranceViewController.swift
+++ Sources/SuperSegurosSDKIOS/Controllers/selectInsuranceViewController.swift
@@ -12,16 +12,14 @@ class selectInsuranceViewController: UIViewController {
 
     let colorSkeleton = UIColor(red: 191/255, green: 148/255, blue: 252/255, alpha: 1)
     
-    let colorBase = UIColor(red: 0.75, green: 0.58, blue: 0.99, alpha: 1.0) // HEX #BF94FC
-    let colorClaro = UIColor(red: 0.85, green: 0.73, blue: 1.0, alpha: 1.0) // Más claro
-    let colorOscuro = UIColor(red: 0.65, green: 0.43, blue: 0.91, alpha: 1.0) // Más oscuro
+    let colorBase = UIColor(red: 0.75, green: 0.58, blue: 0.99, alpha: 1.0)
+    let colorClaro = UIColor(red: 0.85, green: 0.73, blue: 1.0, alpha: 1.0)
     
-    
-  // Mostramos el gradiente animado view.showAnimatedGradientSkeleton(usingGradient: gradient, animation: nil)
+    var skeletonAvailable = true
     
     public static let reusableCell = UINib(nibName: "insuranceTableViewCell", bundle: Bundle.module)
     
-    var items: [String] = ["empty"] {
+    var items: [String] = [] {
         didSet{
             insuranceTableView.reloadData()
         }
@@ -30,6 +28,7 @@ class selectInsuranceViewController: UIViewController {
     @IBOutlet weak var insuranceTableView: UITableView!
     
     @IBAction func backAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
@@ -39,28 +38,41 @@ class selectInsuranceViewController: UIViewController {
         insuranceTableView.register(selectInsuranceViewController.reusableCell, forCellReuseIdentifier: "insuranceTableViewCell")
         insuranceTableView.dataSource = self
         insuranceTableView.delegate = self
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.skeletonAvailable = false
+            self.items.append("ahora si ahi uno")
+        }
+        
     }
 
 }
 extension selectInsuranceViewController: UITableViewDataSource, UITableViewDelegate {
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        if items.count == 0 {
+            return 1
+        
+        } else {
+            return items.count
+        }
+       
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = insuranceTableView.dequeueReusableCell(withIdentifier: "insuranceTableViewCell", for: indexPath) as! insuranceTableViewCell
-//        cell.backGroundView.layer.cornerRadius = 20
-        cell.backGroundView.isSkeletonable = true
-        
-        let gradient = SkeletonGradient(colors: [colorClaro, colorBase, colorClaro])
-        let animation = SkeletonAnimationBuilder().makeSlidingAnimation(withDirection: .leftRight)
-        
-        cell.backGroundView.showAnimatedGradientSkeleton(usingGradient: gradient, animation: animation)
-        
-//        cell.backGroundView.showAnimatedGradientSkeleton(usingGradient: .init(baseColor: gradient), animation: nil, transition: .crossDissolve(0.5))
+
+        if skeletonAvailable {
+            cell.backGroundView.isSkeletonable = skeletonAvailable
+            
+            let gradient = SkeletonGradient(colors: [colorClaro, colorBase, colorClaro])
+            let animation = SkeletonAnimationBuilder().makeSlidingAnimation(withDirection: .leftRight)
+            
+            cell.backGroundView.showAnimatedGradientSkeleton(usingGradient: gradient, animation: animation)
+        }
         
         cell.backGroundView.layer.cornerRadius = 20
         cell.backGroundView.clipsToBounds = true
+        
         cell.selectionStyle = .none
         
         return cell
